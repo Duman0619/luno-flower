54783c8ff6fa7c0cd45f59d20f4d8a9d1cf258c7
Duman0619 <kamaliduman65@gmail.com>
2026-01-29 15:04:10 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 const catalog = document.getElementById("catalog");
+const sortSelect = document.getElementById("sort");
+const filterSelect = document.getElementById("filter");
+
+let currentProducts = [...products];
 
 function renderProducts(list) {
   catalog.innerHTML = "";
@@ -11,8 +15,8 @@ function renderProducts(list) {
       <img src="${p.image}">
       <h3>${p.name}</h3>
       <p>${p.desc}</p>
-      <span class="price">${p.price.toLocaleString()} ₸</span>
-      <a class="btn" href="https://wa.me/77087514281?text=Хочу заказать ${p.name}">
+      <div class="price">${p.price.toLocaleString()} ₸</div>
+      <a class="btn" href="https://wa.me/77087514281?text=Хочу заказать: ${p.name}">
         Заказать
       </a>
     `;
@@ -21,4 +25,31 @@ function renderProducts(list) {
   });
 }
 
+/* сортировка */
+sortSelect.addEventListener("change", () => {
+  let sorted = [...currentProducts];
+
+  if(sortSelect.value === "asc"){
+    sorted.sort((a,b)=>a.price-b.price);
+  }
+
+  if(sortSelect.value === "desc"){
+    sorted.sort((a,b)=>b.price-a.price);
+  }
+
+  renderProducts(sorted);
+});
+
+/* фильтр */
+filterSelect.addEventListener("change", () => {
+  if(filterSelect.value === "all"){
+    currentProducts = [...products];
+  } else {
+    currentProducts = products.filter(p => p.type === filterSelect.value);
+  }
+
+  renderProducts(currentProducts);
+});
+
+/* стартовая загрузка */
 renderProducts(products);
