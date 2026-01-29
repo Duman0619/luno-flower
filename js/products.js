963914a98be9c8742a28aa7bf2d58ca4be76b777
Duman0619 <kamaliduman65@gmail.com>
2026-01-29 14:20:54 +0500

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,1 +1,27 @@
+const products = [
+  {
+    id: 1,
+    name: "Rose Box Mini",
+    price: 12000,
+    type: "box",
+    image: "images/flower1.jpg",
+    desc: "Нежная композиция в коробке"
+  },
+  {
+    id: 2,
+    name: "LUNO Signature",
+    price: 18000,
+    type: "box",
+    image: "images/flower2.jpg",
+    desc: "Авторский бокс от флориста"
+  },
+  {
+    id: 3,
+    name: "Flower Subscription",
+    price: 35000,
+    type: "subscription",
+    image: "images/flower3.jpg",
+    desc: "Подписка на цветы"
+  }
+];
 
