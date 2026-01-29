147a7aaf6c98ab6d5205deb9c394fc524acdc6da
Duman0619 <kamaliduman65@gmail.com>
2026-01-29 14:34:50 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
+const catalog = document.getElementById("catalog");
+
+function renderProducts(list) {
+  catalog.innerHTML = "";
+
+  list.forEach(p => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+      <img src="${p.image}">
+      <h3>${p.name}</h3>
+      <p>${p.desc}</p>
+      <span class="price">${p.price.toLocaleString()} ₸</span>
+      <a class="btn" href="https://wa.me/77087514281?text=Хочу заказать ${p.name}">Заказать</a>
+    `;
+
+    catalog.appendChild(card);
+  });
+}
+
+renderProducts(products);
 
