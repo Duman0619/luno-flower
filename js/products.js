c7b43c089d635f3fb2a1803917fdd36befeb0208
Duman0619 <kamaliduman65@gmail.com>
2026-01-29 17:53:41 +0500

    Update products.js

--- js/products.js
+++ js/products.js
@@ -2,17 +2,17 @@ const products = [
   {
     id: 1,
     name: "Rose Box Mini",
-    desc: "Нежная композиция",
+    desc: "Нежная композиция в коробке",
     price: 12000,
-    image: "images/flower1.jpeg",
+    image: "images/flower1.jpg",
     type: "box"
   },
   {
     id: 2,
     name: "LUNO Signature",
-    desc: "Авторский бокс",
+    desc: "Авторский бокс от флориста",
     price: 18000,
-    image: "images/flower2.jpeg",
+    image: "images/flower2.jpg",
     type: "box"
   },
   {
@@ -20,64 +20,143 @@ const products = [
     name: "Flower Subscription",
     desc: "Подписка на цветы",
     price: 35000,
-    image: "images/flower3.jpeg",
+    image: "images/flower3.jpg",
     type: "subscription"
-  }
+  },
   {
     id: 4,
-    name: "Flower Subscription 3",
-    desc: "Подписка на цвет7ы",
-    price: 356000,
-    image: "images/flower4.jpeg",
-    type: "subscription"
-  }
-{
+    name: "White Pearl",
+    desc: "Белоснежный букет",
+    price: 15000,
+    image: "images/flower4.jpg",
+    type: "bouquet"
+  },
+  {
     id: 5,
-    name: "Flower Subscription 5",
-    desc: "Подписка на цвет8ы",
-    price: 3598000,
-    image: "images/flower5.jpeg",
-    type: "subscription"
-  }
-{
+    name: "Pink Dream",
+    desc: "Розовая нежность",
+    price: 17000,
+    image: "images/flower5.jpg",
+    type: "bouquet"
+  },
+  {
     id: 6,
-    name: "Flower Subscription 6",
-    desc: "Подписка на цвет9ы",
-    price: 359000,
-    image: "images/flower6.jpeg",
-    type: "subscription"
+    name: "Blue Sky",
+    desc: "Голубая композиция",
+    price: 16000,
+    image: "images/flower6.jpg",
+    type: "bouquet"
+  },
+  {
+    id: 7,
+    name: "Golden Sun",
+    desc: "Тёплый солнечный букет",
+    price: 19000,
+    image: "images/flower7.jpg",
+    type: "bouquet"
+  },
+  {
+    id: 8,
+    name: "Lavender Mood",
+    desc: "Лавандовая атмосфера",
+    price: 17500,
+    image: "images/flower8.jpg",
+    type: "bouquet"
+  },
+  {
+    id: 9,
+    name: "Royal Red",
+    desc: "Королевские красные розы",
+    price: 22000,
+    image: "images/flower9.jpg",
+    type: "bouquet"
+  },
+  {
+    id: 10,
+    name: "Snow Queen",
+    desc: "Холодная элегантность",
+    price: 21000,
+    image: "images/flower10.jpg",
+    type: "bouquet"
+  },
+  {
+    id: 11,
+    name: "Spring Kiss",
+    desc: "Весенний поцелуй",
+    price: 14500,
+    image: "images/flower11.jpg",
+    type: "bouquet"
+  },
+  {
+    id: 12,
+    name: "Velvet Rose",
+    desc: "Бархатные розы",
+    price: 20000,
+    image: "images/flower12.jpg",
+    type: "bouquet"
+  },
+  {
+    id: 13,
+    name: "Soft Cloud",
+    desc: "Воздушная композиция",
+    price: 16500,
+    image: "images/flower13.jpg",
+    type: "bouquet"
+  },
+  {
+    id: 14,
+    name: "Bloom Box",
+    desc: "Цветочная коробка",
+    price: 23000,
+    image: "images/flower14.jpg",
+    type: "box"
+  },
+  {
+    id: 15,
+    name: "LUNO Premium Box",
+    desc: "Премиум коробка",
+    price: 28000,
+    image: "images/flower15.jpg",
+    type: "box"
+  },
+  {
+    id: 16,
+    name: "Romantic Touch",
+    desc: "Романтический букет",
+    price: 18500,
+    image: "images/flower16.jpg",
+    type: "bouquet"
+  },
+  {
+    id: 17,
+    name: "Pure Love",
+    desc: "Чистая любовь",
+    price: 19500,
+    image: "images/flower17.jpg",
+    type: "bouquet"
+  },
+  {
+    id: 18,
+    name: "Crystal Bloom",
+    desc: "Кристальная свежесть",
+    price: 20500,
+    image: "images/flower18.jpg",
+    type: "bouquet"
+  },
+  {
+    id: 19,
+    name: "Soft Pink Box",
+    desc: "Нежный бокс",
+    price: 24000,
+    image: "images/flower19.jpg",
+    type: "box"
+  },
+  {
+    id: 20,
+    name: "LUNO Grand",
+    desc: "Большая композиция",
+    price: 32000,
+    image: "images/flower20.jpg",
+    type: "box"
   }
-{
-  id: 7,
-  name: "Blue Sky",
-  desc: "Голубые гортензии",
-  price: 27000,
-  image: "images/flower7.jpeg",
-  type: "bouquet"
-},
-{
-  id: 8,
-  name: "White Pearl",
-  desc: "Белые розы в коробке",
-  price: 23000,
-  image: "images/flower8.jpeg",
-  type: "box"
-},
-{
-  id: 9,
-  name: "Luxury Red",
-  desc: "Красные розы премиум",
-  price: 35000,
-  image: "images/flower9.jpeg",
-  type: "lux"
-},
-{
-  id: 10,
-  name: "Weekly Flowers",
-  desc: "Подписка на цветы (1 месяц)",
-  price: 40000,
-  image: "images/flower10.jpeg",
-  type: "subscription"
-}
-
 ];
